13d5f721ad10668e03589149a4056617c8a60fa2
Daniela-Estupinan <55764801+Daniela-Estupinan@users.noreply.github.com>
2019-10-03 15:17:02 -0500

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Realizar un programa en C que permita ingresar por teclado los grados centígrados y
+Fahrenheit y realice la conversión.
+
+*******************************************************************************/
+
+#include <stdio.h>                                                   //Declaracion de la libreria
+
+void main()                                                         //Declaracion de variables
+{
+   float centigrados=0, fahrenheit=0;                              //Inicializacion de las variables:centigrados y fahrenheit
+   printf("Ingrese grados centigrados:");                         //Impresion en pantalla del mensaje
+   scanf("%f",&centigrados);                                     //Ingresar el valor de la variable centigrados
+   printf("Ingrese grados en fahrenheit:");                     //Impresion en pantalla del mensaje
+   scanf("%f",&fahrenheit);                                    //Ingresar el valor de la variable fahrenheit
+   fahrenheit=(centigrados*9)/5+32;                           // Calculo matemático para convertir de centigrados a fahrenheit
+   centigrados=(fahrenheit-32*5)/9;                          //Calculo matemático para convertir de fahrenheit a centigrados
+   printf("Grados en fahrenheit:%.2f",fahrenheit);          // Impresion del valor con dos decimales de fahrenheit
+   printf("\t\tGrados en centigrados:%.2f",centigrados);   // Impresion del valor con dos decimales de centigrados
+   }
